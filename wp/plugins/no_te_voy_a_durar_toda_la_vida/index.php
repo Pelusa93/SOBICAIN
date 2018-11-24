05e23fdb96286509ce35f1f85d6782c4d4c6d08c
nullpointer <rbenal@gmail.com>
2018-11-24 01:29:03 -0300

    mejoras al .gitignore

---
+++ wp/plugins/no_te_voy_a_durar_toda_la_vida/index.php
@@ -0,0 +1,9 @@
+<?php
+	/*
+		Plugin Name: Calimero
+		Plugin URI: http://er-guevo.com
+		Description: No te voy a durar toda la vida
+		Author: Nullpointer
+		Version: 1.0
+		Author URI: http://www.rbenal.com
+	 */
